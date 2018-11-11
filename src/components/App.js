bea70c914b18d8700c7e9586c175af1e1ce0ad82
Sunset Sarsaparilla <ostgals@gmail.com>
2018-11-11 21:36:16 +0200

    common apollo setup and boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>GitHub GraphQL Client (powered by Apollo)</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
